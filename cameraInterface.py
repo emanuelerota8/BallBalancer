226ae91208076c8d724ed867554b127b78324495
Emanuele Rota <emanuelerota8@gmail.com>
2021-11-05 11:56:00 +0100

    tracking ball center

---
+++ cameraInterface.py
@@ -0,0 +1,63 @@
+import cv2
+import time
+import numpy as np
+
+# define a video capture object
+vid = cv2.VideoCapture(2)
+
+scale = 0.3
+minRadius = int(np.ceil(1 * scale))
+maxRadius = int(np.ceil(30 * scale))
+
+xCenter = []
+yCenter = []
+
+while True:
+    ret, frame = vid.read()
+
+    frame = cv2.resize(
+        frame, (int(frame.shape[1]*scale), int(frame.shape[0]*scale)))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    bilateral = cv2.bilateralFilter(gray, 15, 75, 75)
+
+    edges = cv2.Canny(image=bilateral, threshold1=100,
+                      threshold2=200)  # Canny Edge
+
+    circles = cv2.HoughCircles(image=edges,
+                               method=cv2.HOUGH_GRADIENT,
+                               dp=1.5,
+                               minDist=2*minRadius,
+                               param1=1,
+                               param2=25,
+                               minRadius=minRadius,
+                               maxRadius=maxRadius
+                               )
+
+    if circles is not None:
+        circles = circles[0, 0]
+        xCenter.append(int(circles[0]))
+        yCenter.append(int(circles[1]))
+        # processed = cv2.circle(
+        #     frame, (int(circles[0]), int(circles[1])), int(circles[2]), (255, 0, 0), 2)
+
+        for idx in range(len(xCenter)):
+            processed = cv2.circle(
+                frame, (int(xCenter[idx]), int(yCenter[idx])), 1, (255, 255, 0), 2)
+
+    else:
+        print("Ball not found")
+        processed = frame
+
+    cv2.namedWindow('frame', cv2.WINDOW_KEEPRATIO)
+    cv2.imshow('frame', processed)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
