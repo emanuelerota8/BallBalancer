835cf8bab23747e7e24cd4eede576a2bb8a0a632
Emanuele Rota <emanuelerota8@gmail.com>
2021-12-03 17:56:29 +0100

    first impl with pid controller

--- cameraInterface.py
+++ cameraInterface.py
@@ -1,11 +1,13 @@
 import cv2
 import time
 import numpy as np
+from simple_pid import PID
+from pinInterface import setupServo,goToAngle
 
 # define a video capture object
-vid = cv2.VideoCapture(2)
+vid = cv2.VideoCapture(0)#2 on laptop
 
-scale = 1
+scale = 0.3
 minRadius = int(np.ceil(10 * scale))
 maxRadius = int(np.ceil(20 * scale))
 
@@ -15,15 +17,21 @@ yCenter = []
 actualX = 0
 actualY = 0
 
+p = setupServo()
+
 while True:
     ret, frame = vid.read()
+    
+    frame = frame[100:400,100:550]
+    #print(frame.shape)
 
     frame = cv2.resize(
         frame, (int(frame.shape[1]*scale), int(frame.shape[0]*scale)))
+    
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    bilateral = cv2.bilateralFilter(gray, 15, 75, 75)
+    bilateral = cv2.bilateralFilter(gray, 5, 5, 75)
 
     edges = cv2.Canny(image=bilateral, threshold1=100,
                       threshold2=200)  # Canny Edge
@@ -59,6 +67,20 @@ while True:
 
     processed = cv2.circle(
         frame, (int(actualX), int(actualY)), 1, (255, 255, 0), 2)
+    
+    #PID controller
+    pid = PID(1, 0.1, 0.05, setpoint=68)
+    control = pid(actualX)
+    print("control: "+ str(control))
+    print("actual: "+ str(actualX))
+    print("diff: "+ str(actualX-control))
+    
+    
+    
+    trueAction =((control - (-100))/(100+100) )* (2)
+    goToAngle(p,trueAction)
+    print("true actionn" + str(trueAction))
+
 
     cv2.namedWindow('frame', cv2.WINDOW_KEEPRATIO)
     cv2.imshow('frame', processed)
--- pinInterface.py
+++ pinInterface.py
@@ -1,35 +1,53 @@
 import RPi.GPIO as GPIO
 import time
 
+ITERATIONS = 2
+LOWER = 6.5
+UPPER = 8.5
+START = 7.5
+SLEEP = 0.5
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN,GPIO.OUT)
+def setupServo():
+    servoPIN = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servoPIN,GPIO.OUT)
 
-p = GPIO.PWM(servoPIN,50)
-p.start(0)
+    p = GPIO.PWM(servoPIN,50)
+    p.start(0)
+    time.sleep(0.1)
+    return p
 
-if True:
-    for i in range(0,1,1):
-        time.sleep(1)
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(12.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(2.5)
-        time.sleep(0.5)
-      
-        #p.ChangeDutyCycle(5)
+def rotateOverFullRange(p,increment):
+    i=0
+    count = START
+    if False:
+        while i< ITERATIONS:
+            count+=1
+            print(count)
+            p.ChangeDutyCycle(count)
+            time.sleep(0.5)
+            if count == START:
+                i+=increment
+            if count >= UPPER or count <=LOWER:
+                increment*=-1
 
-p.stop()
-GPIO.cleanup()
+def goToAngle(p,angle):
+    p.ChangeDutyCycle(angle)
+    time.sleep(SLEEP)
+    pass
+
+
+
+if __name__ == "__main__":
+    p = setupServo()
+    #rotateOverFullRange(p,1):
+    for i in range(3):
+        goToAngle(p,LOWER)
+        goToAngle(p,START)
+        goToAngle(p,UPPER)
+
+
+
+    time.sleep(1)
+    p.stop()
+    GPIO.cleanup()
