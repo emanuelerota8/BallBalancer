6f1fd6e47149c7c0b602b01fa1305b3df78ddf04
Luca Rillosi <luca.rillosi+git@gmail.com>
2021-12-10 22:33:52 +0100

    improve debugging preview

--- finder.py
+++ finder.py
@@ -9,49 +9,52 @@ def preprocess(img):
     return img
 
 
-def filterOutsidePlate(img, debug: bool = False):
-    orig = img.copy()
-    # Canny
-    img = cv2.Canny(img, 35, 35/3)
-    # Dilate
+def filterOutsidePlate(img, debug_img=None):
+    # Highlight plate in white with thick black edges
+    mask = cv2.Canny(img, 35, 35/3)
     kernel = np.ones((8, 8))
-    img = cv2.morphologyEx(img, cv2.MORPH_DILATE, kernel)
-    img = np.bitwise_not(img)
-    # Find biggest contour
-    cnts, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel)
+    mask = np.bitwise_not(mask)
+    # Get convex hull of the biggest white area
+    cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
     cnt = cnts[0]
     cnt = cv2.convexHull(cnt, False)
-    # Draw mask
-    mask = cv2.drawContours(img * 0, [cnt], -1, 255, -1)
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel)
+    # Set everything outside the plate as pure white
+    mask = cv2.drawContours(mask * 0, [cnt], -1, 255, -1)
+    # Render image for debugging purposes
+    if debug_img is not None:
+        if len(debug_img.shape) == 2:
+            debug_img = cv2.cvtColor(debug_img, cv2.COLOR_GRAY2BGR)
+        plate_area = debug_img * 0
+        plate_area[mask == 255] = [0, 255, 75]
+        debug_img = cv2.addWeighted(debug_img, 1, plate_area, .5, 0)
+    img[mask != 255] = 255
+    return img, debug_img
 
-    # draw preview
-    preview = None
-    if debug:
-        preview = orig.copy()
-        preview = cv2.cvtColor(preview, cv2.COLOR_GRAY2BGR)
-        preview = cv2.drawContours(preview, [cnt], -1, (0, 255, 75), -1)
-    orig[mask != 255] = 0
-    return orig, preview
 
-
-def findball(img):
-    img[img == 0] = 255
+def findball(img, debug_img=None):
+    # Find darkest pixels and erode them a bit to
+    # cancel out any line or stick inside the image
     _, img = cv2.threshold(img, np.median(img)*.5, 255, cv2.THRESH_BINARY_INV)
-
     kernel = np.ones((8, 8))
     img = cv2.morphologyEx(img, cv2.MORPH_ERODE, kernel)
-
+    # Find average pixel coords, if there is any
     idxs = np.argwhere(img == 255)
-
     cx = cy = None
     if len(idxs) > 0:
         cy, cx = [int(val) for val in np.mean(idxs, axis=0)]
-        img = cv2.line(img, (cx, 0), (cx, img.shape[0]), (255, 255, 255), thickness=2)
-        img = cv2.line(img, (0, cy), (img.shape[1], cy), (255, 255, 255), thickness=2)
+        # Render image  with crosshair for debugging purposes
+        if debug_img is not None:
+            if len(debug_img.shape) == 2:
+                debug_img = cv2.cvtColor(debug_img, cv2.COLOR_GRAY2BGR)
+            ball_area = debug_img * 0
+            debug_img[img == 255] = [255, 0,  0]
+            # debug_img = cv2.addWeighted(debug_img, 1, ball_area, 1, 0)
+            debug_img = cv2.line(debug_img, (cx, 0), (cx, debug_img.shape[0]), (255, 255, 255), thickness=2)
+            debug_img = cv2.line(debug_img, (0, cy), (debug_img.shape[1], cy), (255, 255, 255), thickness=2)
 
-    return cx, cy, img
+    return cx, cy, debug_img
 
 
 def main():
@@ -66,9 +69,9 @@ def main():
         i += 1
         start_time = time.time()
         cv2.imshow('window-name', frame)
-        frame, preview = filterOutsidePlate(frame, debug=True)
+        frame, preview = filterOutsidePlate(frame, debug_img=frame)
 
-        cx, cy, preview = findball(frame)
+        cx, cy, preview = findball(frame, debug_img=preview)
 
         cv2.imshow('window-name2', preview)
         if cv2.waitKey(10) & 0xFF == ord('q'):
